e6df9100c214c40e7a1bc56ba4e0b02606583cb9
Justine Okolodkoff <j.okolodkoff@gmail.com>
2020-08-04 12:26:05 +0200

    rails new

---
+++ db/migrate/20200804100225_add_task_to_list.rb
@@ -0,0 +1,9 @@
+class AddTaskToList < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.boolean :done
+      t.timestamps
+    end
+  end
+end
